079e519071c6c91352f04ff365669a41826e8278
Xavier Watkins <xavier.watkins@gmail.com>
2018-07-05 16:48:45 +0100

    Updated main exports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Tile from './src/ui/components/tile';
 import Paginator from './src/ui/components/pagination';
 import TreeSelect from './src/ui/components/tree-select';
 
-export {
+module.exports = {
   Tile,
   Paginator,
   TreeSelect,
