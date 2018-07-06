0d16c6ab8aded99a1ce16fd02dc61485fb6fdcd8
Xavier Watkins <xavier.watkins@gmail.com>
2018-07-06 14:22:25 +0100

    Set namespace via props

--- src/ui/components/components.js
+++ src/ui/components/components.js
@@ -7,7 +7,10 @@ const components = [{
   name: 'Tile',
   component: Tile,
   function: 'Provide a sneak peak and navigate to a searchable data section of the website.',
-  purpose: 'Advertise a specific dataset of the website and provide searchable access to it.'
+  purpose: 'Advertise a specific dataset of the website and provide searchable access to it.',
+  props: {
+    namespace: 'uniref'
+  }
 }, {
   name: 'Pagination',
   component: PaginationSnippet,
--- src/ui/components/tile.jsx
+++ src/ui/components/tile.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import '../../../dist/components/tile.css';
 
 const Tile = props => (
-  <div className="tile">
+  <div className={props.namespace ? `tile tile-${props.namespace}` : 'tile'}>
     <h3 className="tile__header">{props.title}</h3>
     <p className="tile__content">{props.description}</p>
   </div>
@@ -13,11 +13,13 @@ const Tile = props => (
 Tile.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
+  namespace: PropTypes.string,
 };
 
 Tile.defaultProps = {
   title: 'Tile title',
   description: 'This is a short description of what the resource is/provides.',
+  namespace: '',
 };
 
 export default Tile;
