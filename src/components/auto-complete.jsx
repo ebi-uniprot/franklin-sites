6aaeb272be8183867956093189f7a3c06ea3c211
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-10-05 16:46:22 +0100

    Create copy of hover style for up/down key presses; bounded up/down key presses functional.

--- src/components/auto-complete.jsx
+++ src/components/auto-complete.jsx
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import '../../dist/components/treeSelect.css';
 import '../../dist/components/dropdown.css';
+import '../../dist/components/auto-complete.css';
 
 class AutoComplete extends Component {
   constructor(props) {
     super(props);
+    const { data } = this.props
     this.state = {
       textInputValue: '',
-      data: this.props.data,
       showDropdown: false,
+      hoverIndex: -1,
+      data,
     };
     this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnKeyDown = this.handleOnKeyDown.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   filterOptions(items, query) {
@@ -39,27 +43,61 @@ class AutoComplete extends Component {
     return <Fragment>{prestring}<b>{highlight}</b>{poststring}</Fragment>;
   }
 
-  buildOptions(data, highlightSubstring) {
+  buildOptions(data, substringToHighlight) {
     return (
       <ul className={open ? 'open' : ''}>
-        {data.map(node => (
+        {data.map((node, index) => (
           <li key={node.label}>
-            <span>
-              {!!highlightSubstring ? this.highlightSubstring(node.label, highlightSubstring) : node.label}
+            <span
+              onClick={e => this._handleNodeClick(node, e)}
+              className={this.state.hoverIndex === index ? 'hover' : ''}
+            >
+              {!!substringToHighlight ? this.highlightSubstring(node.label, substringToHighlight) : node.label}
             </span>
           </li>
         ))}
       </ul>
     );
   }
+
+  _handleNodeClick(node, e) {
+    console.log('in handleNodeClick');
+      // this.setState({node.value})
+      // this.toggleTreeSelect();
+    
+  }
+
+  handleOnKeyDown(event) {
+    if(event.key == 'Enter') {
+      console.log('enter press here! ')
+    }
+    else if(event.key == 'ArrowUp') {
+      console.log('ArrowUp press here! ')
+      let { hoverIndex } = this.state;
+      hoverIndex = Math.max(-1, hoverIndex - 1)
+      this.setState({hoverIndex})
+    }
+    else if(event.key == 'ArrowDown') {
+      console.log('ArrowDown press here! ')
+      let { hoverIndex, data, textInputValue } = this.state;
+      const options = this.filterOptions(data, textInputValue);
+      hoverIndex = Math.min(options.length - 1, hoverIndex + 1)
+      this.setState({hoverIndex})
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
   render() {
     const { textInputValue, showDropdown, data } = this.state;
     return (
       <div className="dropdown">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={textInputValue} onChange={this.handleChange} />
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" value={textInputValue} onChange={this.handleChange} onKeyDown={this.handleOnKeyDown} />
         </form>
-        <div className={showDropdown ? 'dropdown-menu dropdown-menu-open' : 'dropdown-menu'} style={{top:39, left:1}}>
+        <div className={showDropdown ? 'autcomplete-menu dropdown-menu-open' : 'autcomplete-menu'}>
           {this.buildOptions(this.filterOptions(data, textInputValue), textInputValue)}
         </div>
       </div>
