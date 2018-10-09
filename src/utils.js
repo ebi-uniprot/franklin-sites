b236d218cd5d191d45be9d67d8ef33d876ef2f07
Xavier Watkins <xavier.watkins@gmail.com>
2018-10-09 13:37:41 +0100

    Add generic path flattening util
    (cherry picked from commit e023499560539434ae69951960a7be81836110cf)

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,20 @@
-export default function findLastSubstringIgnoreCase(string, substring) {
+export function findLastSubstringIgnoreCase(string, substring) {
   return string.toLowerCase().lastIndexOf(substring.toLowerCase());
 }
+
+export const getFlattenedPaths = (currentItems, id, path = []) => {
+  let flattened = [];
+  currentItems.forEach((node) => {
+    const { items, ...thisNode } = node;
+    const nodePath = [...path, thisNode];
+    if (items) {
+      const result = getFlattenedPaths(items, id, nodePath);
+      if (result.length) {
+        flattened = [...flattened, ...result];
+      }
+    } else if (!id || thisNode.label === id) {
+      flattened = [...flattened, nodePath];
+    }
+  });
+  return flattened;
+};
