1fa2aa88d20c5798c1649dfab11bf61597107590
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-06 11:14:30 +0000

    Export Autocomplete in index.js.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Tile } from './tile';
 export { default as Header } from './header';
 export { default as TreeSelect } from './tree-select';
+export { default as Autocomplete } from './autocomplete';
