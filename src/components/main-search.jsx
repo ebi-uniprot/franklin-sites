a9cd431db75fe8ea254088d63e195dccf3b0f2b9
Xavier Watkins <xavier.watkins@gmail.com>
2018-11-08 11:56:13 +0100

    Adding main search

--- src/components/main-search.jsx
+++ src/components/main-search.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Select from './select';
 import '../../dist/components/main-search.css';
-import '../../dist/components/dropdown.css';
 
 const showDropdown = true;
 
 const MainSearch = () => (
   <div className="main-search">
-    <div className="dropdown-container">
-      <button id="namespace-selector" type="button" className="button dropdown">
-        UniProtKB
-      </button>
-      <div className={showDropdown ? 'dropdown-menu dropdown-menu-open' : 'dropdown-menu'}>
-        This is some content
-      </div>
-    </div>
+    <Select label="UniProtKB">
+      <ul>
+        <li>
+          <button>UniProtKB - the UniProt knowledgebase</button>
+        </li>
+        <li>
+          <button>UniRef</button>
+        </li>
+        <li>
+          <button>UniParc</button>
+        </li>
+        <li>
+          <button>Proteomes</button>
+        </li>
+        <li>
+          <button>Publications</button>
+        </li>
+        <li>
+          <button>Keywords</button>
+        </li>
+      </ul>
+    </Select>
     <input type="text" className="main-search__input" />
     <button type="button" className="button">
       Search
