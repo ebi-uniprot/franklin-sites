4997c99414bc5fc41bad9530880dd6371ea17564
Xavier Watkins <xavier.watkins@gmail.com>
2019-02-12 14:12:10 +0000

    Add page intro pattern

---
+++ src/components/page-intro.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { formatLargeNumber } from '../utils';
+import '../styles/components/page-intro.scss';
+import ChevronDown from '../svg/chevron-down.svg';
+import ChevronUp from '../svg/chevron-up.svg';
+
+class PageIntro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContent: false,
+    };
+  }
+
+  render() {
+    const {
+      title, resultsCount, children, links,
+    } = this.props;
+    const { showContent } = this.state;
+    return (
+      <div className="page-intro">
+        <h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ showContent: !showContent })}
+            className="dropdown"
+          >
+            {showContent ? <ChevronUp /> : <ChevronDown />}
+            {' '}
+            {title}
+          </button>
+          {resultsCount > 0 && (
+          <small>
+            {' '}
+            {formatLargeNumber(resultsCount)}
+            {' '}
+results
+          </small>
+          )}
+        </h2>
+
+        <div className="intro-content" style={{ display: showContent ? 'block' : 'none' }}>
+          {children}
+          <div className="intro-links">
+            {links.map(link => (
+              <Link to={link.destination} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+PageIntro.propTypes = {
+  title: PropTypes.string.isRequired,
+  resultsCount: PropTypes.number,
+  children: PropTypes.node.isRequired,
+  links: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+PageIntro.defaultProps = {
+  resultsCount: 0,
+  links: [],
+};
+
+export default PageIntro;
--- webpack.library.js
+++ webpack.library.js
@@ -50,6 +50,20 @@ module.exports = [
             },
           ],
         },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+            {
+              loader: 'react-svg-loader',
+              options: {
+                jsx: true, // true outputs JSX tags
+              },
+            },
+          ],
+        },
       ],
     },
   },
