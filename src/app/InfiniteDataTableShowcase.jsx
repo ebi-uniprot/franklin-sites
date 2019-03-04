2a22f2da256a1410cbf2096990f19baa8ff5119a
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-03-04 17:02:50 +0000

    Simulate data fetching with generateDataRows; lighten unselected sort button.

--- src/app/InfiniteDataTableShowcase.jsx
+++ src/app/InfiniteDataTableShowcase.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import loremIpsum from 'lorem-ipsum';
+import v1 from 'uuid';
 import DefaultPageLayout from './layout/DefaultPageLayout';
 import InfiniteDataTable from '../components/infinite-data-table';
 
@@ -15,62 +17,62 @@ const propData = {
     {
       label: 'Column 1',
       name: 'col1',
-      render: row => <span>{row.fieldValue1.value}</span>,
+      render: row => <span>{row.col1.value}</span>,
       sortable: true,
     },
     {
       label: 'Column 2',
       name: 'col2',
-      render: row => <span>{row.fieldValue2.value}</span>,
+      render: row => <span>{row.col2.value}</span>,
       sorted: 'ascend',
       sortable: true,
     },
   ],
   idKey: 'accessionId',
-  data: [
-    {
-      accessionId: 'blah1',
-      fieldValue1: {
-        value: 'Some data 1',
-      },
-      fieldValue2: {
-        value: 'Some data 2',
-      },
-    },
-    {
-      accessionId: 'blah2',
-      fieldValue1: {
-        value: 'Some data A',
-      },
-      fieldValue2: {
-        value: 'Some data B',
-      },
-    },
-    {
-      accessionId: 'blah3',
-      fieldValue1: {
-        value: 'Some data α',
-      },
-      fieldValue2: {
-        value: 'Some data β',
-      },
-    },
-  ],
 };
 
 class InfiniteDataTableShowcaseContent extends Component {
   constructor(props) {
     super(props);
-    this.state = { a: 1 };
+    this.state = { data: [] };
+    this.generateDataRows();
+  }
+
+  generateDataRows(numberDataPoints = 10, numberWords = 10, sleepDuration = 2) {
+    const { columns, idKey } = this.props;
+    const data = Array(numberDataPoints)
+      .fill(null)
+      .map(() => {
+        const dataPoint = { [idKey]: v1() };
+        columns.forEach((column) => {
+          dataPoint[column.name] = { value: loremIpsum({ count: numberWords, units: 'words' }) };
+        });
+        return dataPoint;
+      });
+    setTimeout(() => {
+      this.setState({ data });
+    }, sleepDuration * 1000);
   }
 
   render() {
-    return <InfiniteDataTable {...propData} />;
+    const { data } = this.state;
+
+    return (
+      <Fragment>
+        <button type="submit" className="button">
+          Append more rows
+        </button>
+        <InfiniteDataTable {...this.props} data={data} />
+      </Fragment>
+    );
   }
 }
 
 const InfiniteDataTableShowcase = () => (
-  <DefaultPageLayout title="Franklin - Data Table" content=<InfiniteDataTableShowcaseContent /> />
+  <DefaultPageLayout
+    title="Franklin - Data Table"
+    content=<InfiniteDataTableShowcaseContent {...propData} />
+  />
 );
 
 export default InfiniteDataTableShowcase;
