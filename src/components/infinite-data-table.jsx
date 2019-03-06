a84efbe6d33e48f2de4bc2cd422220ead637e29c
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-03-06 11:30:38 +0000

    Initial attempt at using react-virtualized's MultiGrid for the infinite-data-table component.

--- src/components/infinite-data-table.jsx
+++ src/components/infinite-data-table.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../styles/components/data-table.scss';
+import {
+  CellMeasurer,
+  CellMeasurerCache,
+  MultiGrid,
+  AutoSizer,
+  InfiniteLoader,
+} from 'react-virtualized';
+
+const defaultWidth = 200;
+const resultsTableRowIndex = {
+  label: '#',
+  name: 'resultsTableRowIndex',
+  render: (row, index) => index,
+};
 
 class InfiniteDataTable extends Component {
+  cache = new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 60,
+    // minHeight: 60,
+  });
+
   constructor(props) {
     super(props);
     this.handleHeaderKeyPress = this.handleHeaderKeyPress.bind(this);
+    this.getColumnWidth = this.getColumnWidth.bind(this);
+    this.cellRenderer = this.cellRenderer.bind(this);
+    this.isRowLoaded = this.isRowLoaded.bind(this);
   }
 
   static getRowClassName(index, selected = false) {
@@ -29,6 +51,75 @@ class InfiniteDataTable extends Component {
     }
   }
 
+  cellRenderer({
+    columnIndex, rowIndex, key, parent, style,
+  }) {
+    let { columns } = this.props;
+    const {
+      data, fixedRowCount, fixedColumnCount, showHeader, showRowNumbers,
+    } = this.props;
+    let className = '';
+    let cellContent;
+
+    if (showRowNumbers) {
+      columns = [resultsTableRowIndex, ...columns];
+    }
+
+    if (fixedColumnCount && columnIndex < fixedColumnCount) {
+      className += ' frozen-column';
+    }
+
+    if (fixedRowCount && rowIndex < fixedRowCount) {
+      className += ' frozen-row';
+    }
+
+    const ri = rowIndex - Number(showHeader);
+    const column = columns[columnIndex];
+    if (ri === -1) {
+      cellContent = 'label' in column ? <b>{column.label}</b> : `${columnIndex} has no label`;
+    } else {
+      const row = data[ri];
+      if (!className.includes('frozen-column')) {
+        className += ri % 2 ? ' table-row-odd' : ' table-row-even';
+      }
+      if (!row) {
+        cellContent = 'loading...';
+      } else if ('render' in column) {
+        cellContent = column.render(row, rowIndex);
+      } else {
+        cellContent = <div className="warning">{`${columnIndex} has no render method`}</div>;
+      }
+    }
+    // console.log(style);
+    return (
+      <CellMeasurer
+        cache={this.cache}
+        columnIndex={columnIndex}
+        key={key}
+        parent={parent}
+        rowIndex={ri}
+      >
+        <div style={style}>{cellContent}</div>
+      </CellMeasurer>
+    );
+  }
+
+  getColumnWidth({ index }) {
+    let { columns } = this.props;
+    const { showRowNumbers } = this.props;
+    if (showRowNumbers) {
+      columns = [resultsTableRowIndex, ...columns];
+    }
+    const column = columns[index];
+    return 'width' in column ? column.width : defaultWidth;
+  }
+
+  isRowLoaded({ index }) {
+    const { data, showRowNumbers } = this.props;
+    console.log(index, !!data[index]);
+    return !!data[index];
+  }
+
   render() {
     const {
       columns,
@@ -38,58 +129,53 @@ class InfiniteDataTable extends Component {
       onSelect,
       onHeaderClick,
       idKey,
+      onLoadMoreRows,
+      showHeader,
+      fixedColumnCount,
+      fixedRowCount,
+      totalNumberRows,
+      numberResultsPerRequest,
+      showRowNumbers,
     } = this.props;
+    const rowCount = Math.min(
+      totalNumberRows + Number(showHeader),
+      data.length + numberResultsPerRequest - 1 + Number(showHeader),
+    );
+    console.log(rowCount, totalNumberRows, data.length);
     return (
-      <div className="data-table">
-        <div className="table-head">
-          <div className="table-row">
-            {selectable && (
-              <div className="table-header-checkbox" key="select">
-                {''}
-              </div>
+      <InfiniteLoader
+        loadMoreRows={onLoadMoreRows}
+        isRowLoaded={this.isRowLoaded}
+        rowCount={rowCount}
+        threshold={1}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <AutoSizer>
+            {({ width, height }) => (
+              <MultiGrid
+                cellRenderer={this.cellRenderer}
+                columnCount={columns.length + Number(showRowNumbers)}
+                width={width}
+                height={height}
+                // columnWidth={this.getColumnWidth}
+                columnWidth={300}
+                rowHeight={this.cache.rowHeight}
+                deferredMeasurementCache={this.cache}
+                // fixedColumnCount={fixedColumnCount}
+                // fixedRowCount={fixedRowCount}
+                ref={registerChild}
+                rowCount={rowCount}
+                scrollToRow={data.length <= numberResultsPerRequest ? 0 : undefined}
+                onSectionRendered={({ rowStartIndex, rowStopIndex }) => onRowsRendered({
+                  startIndex: rowStartIndex,
+                  stopIndex: rowStopIndex,
+                })
+                }
+              />
             )}
-            {columns.map(column => (column.sortable ? (
-              <button
-                type="button"
-                key={column.name}
-                className={InfiniteDataTable.getSortableColumnClassName(column)}
-                onClick={() => onHeaderClick(column.name)}
-                onKeyPress={() => this.handleHeaderKeyPress(column.name)}
-              >
-                {column.label}
-              </button>
-            ) : (
-              <div key={column.name} className="table-header">
-                {column.label}
-              </div>
-            )))}
-          </div>
-        </div>
-        <div className="table-body">
-          {data.map((row, i) => (
-            <div
-              className={InfiniteDataTable.getRowClassName(i, row[idKey] in selected)}
-              key={row[idKey]}
-            >
-              {selectable && (
-                <div className="table-data-checkbox" key={`${row[idKey]}_select`}>
-                  <input
-                    id={row[idKey]}
-                    type="checkbox"
-                    onChange={() => onSelect(row[idKey])}
-                    checked={row[idKey] in selected}
-                  />
-                </div>
-              )}
-              {columns.map(column => (
-                <div className="table-data" key={`${row[idKey]}_${column.name}`}>
-                  {column.render(row)}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </div>
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
     );
   }
 }
