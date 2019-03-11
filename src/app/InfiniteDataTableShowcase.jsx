eb060786a2e80840d9556697f5c4c5a3838ab4eb
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-03-11 14:31:14 +0000

    Attempt at using react-virtualized table - some of the row heights are still randomly too small.

--- src/app/InfiniteDataTableShowcase.jsx
+++ src/app/InfiniteDataTableShowcase.jsx
@@ -6,11 +6,11 @@ import InfiniteDataTable from '../components/infinite-data-table';
 
 const propData = {
   selectable: true,
-  fixedColumnCount: 0,
-  fixedRowCount: 0,
+  fixedColumnCount: 1,
+  fixedRowCount: 1,
   showHeader: true,
   numberResultsPerRequest: 10,
-  totalNumberRows: 20,
+  totalNumberRows: 40,
   showRowNumbers: true,
   columns: [
     {
@@ -18,7 +18,7 @@ const propData = {
       name: 'col1',
       render: row => <span>{row.col1.value}</span>,
       sortable: true,
-      width: 100,
+      width: 200,
     },
     {
       label: 'Column 2',
@@ -26,7 +26,7 @@ const propData = {
       render: row => <span>{row.col2.value}</span>,
       sorted: 'ascend',
       sortable: true,
-      width: 200,
+      width: 300,
     },
   ],
   idKey: 'accessionId',
@@ -36,6 +36,9 @@ class InfiniteDataTableShowcaseContent extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [], selected: {} };
+  }
+
+  componentDidMount() {
     this.generateDataRows();
   }
 
@@ -47,17 +50,24 @@ class InfiniteDataTableShowcaseContent extends Component {
     console.log(columnName);
   }
 
-  generateDataRows(numberWords = 10, sleepDuration = 0) {
+  generateDataRows(numberWords = 10, sleepDuration = 1) {
     const {
       columns, idKey, numberResultsPerRequest, totalNumberRows,
     } = this.props;
     const { data } = this.state;
+    console.log(columns, idKey, numberResultsPerRequest, totalNumberRows);
     const moreData = Array(Math.min(numberResultsPerRequest, totalNumberRows - data.length))
       .fill(null)
       .map(() => {
         const dataPoint = { [idKey]: v1() };
         columns.forEach((column) => {
-          dataPoint[column.name] = { value: loremIpsum({ count: numberWords, units: 'words' }) };
+          dataPoint[column.name] = {
+            value: loremIpsum({
+              sentenceLowerBound: 2,
+              sentenceUpperBound: 100,
+              units: 'sentences',
+            }),
+          };
         });
         return dataPoint;
       });
@@ -67,7 +77,7 @@ class InfiniteDataTableShowcaseContent extends Component {
   }
 
   render() {
-    const { selected } = this.state;
+    const { selected, data } = this.state;
     const {
       selectable,
       columns,
@@ -79,131 +89,9 @@ class InfiniteDataTableShowcaseContent extends Component {
       showHeader,
       numberResultsPerRequest,
     } = this.props;
-    const data = [
-      {
-        accessionId: '949e4f58-9d94-464d-bdcd-a0fb818dc5b5',
-        col1: { value: 'officia aliquip consectetur eu cillum magna officia sit culpa in' },
-        col2: { value: 'sunt occaecat culpa anim occaecat ut voluptate commodo officia ex' },
-      },
-      {
-        accessionId: '32f9a14e-7b0b-40b8-9433-872020d62e70',
-        col1: { value: 'id officia officia aute sit fugiat aliquip ad laborum elit' },
-        col2: {
-          value: 'cillum exercitation sint tempor consequat et ullamco commodo proident dolore',
-        },
-      },
-      {
-        accessionId: '2dbcfa84-a778-4e3f-911a-8ff78c1de4a9',
-        col1: {
-          value: 'laboris sit enim laborum nulla incididunt sit voluptate voluptate cupidatat',
-        },
-        col2: { value: 'ex ad non est occaecat adipisicing irure elit excepteur anim' },
-      },
-      {
-        accessionId: 'e12462b2-0c2f-4350-894c-b2826281c4b6',
-        col1: { value: 'ut laboris ullamco commodo anim commodo enim esse id do' },
-        col2: {
-          value: 'et nulla laboris voluptate irure consectetur occaecat deserunt proident in',
-        },
-      },
-      {
-        accessionId: 'edcdab5d-7c71-40ec-bcf8-65d483510c42',
-        col1: { value: 'occaecat nulla duis aute labore ut reprehenderit sunt duis sit' },
-        col2: { value: 'minim incididunt fugiat aute ut labore labore dolore eiusmod sit' },
-      },
-      {
-        accessionId: '92d3c68d-17c7-4c79-b775-42681d681a41',
-        col1: { value: 'officia quis magna voluptate magna adipisicing et elit voluptate minim' },
-        col2: { value: 'aliqua cillum adipisicing ad tempor ullamco sunt aliquip esse qui' },
-      },
-      {
-        accessionId: '2e521ff8-9d38-4bcb-88a5-f64a05f426c4',
-        col1: {
-          value:
-            'consectetur consequat incididunt incididunt nostrud sit enim adipisicing dolor occaecat',
-        },
-        col2: { value: 'duis sunt minim deserunt ad consectetur cupidatat dolor ipsum ex' },
-      },
-      {
-        accessionId: 'f0ffb9a4-ff67-46b7-88d1-21b364ccfffb',
-        col1: { value: 'id labore occaecat magna proident consequat ex ex pariatur non' },
-        col2: {
-          value:
-            'voluptate aliquip reprehenderit ut aliquip laboris aliquip occaecat adipisicing reprehenderit',
-        },
-      },
-      {
-        accessionId: 'ef0e763f-bc27-455a-b6bd-b6ed8e03a92a',
-        col1: {
-          value: 'id dolor velit cupidatat sint nostrud nulla deserunt reprehenderit fugiat',
-        },
-        col2: { value: 'nostrud non do deserunt do incididunt dolor ad minim veniam' },
-      },
-      {
-        accessionId: '8c63c361-a698-4edd-9664-8a17dfbab2bb',
-        col1: { value: 'adipisicing sit ut minim cupidatat mollit culpa minim cupidatat occaecat' },
-        col2: { value: 'nulla voluptate eiusmod consequat cillum aliquip aliqua aliqua aute aute' },
-      },
-      {
-        accessionId: 'fdbc6e62-cf9f-48bc-b9a7-e7abc61218a1',
-        col1: { value: 'eu aute dolore enim et tempor pariatur amet do commodo' },
-        col2: { value: 'labore adipisicing laboris aliqua excepteur fugiat tempor ea anim aute' },
-      },
-      {
-        accessionId: 'b2a642b1-2867-4e95-960d-2cd3ab65d814',
-        col1: { value: 'nisi est velit in eu est pariatur Lorem nisi laborum' },
-        col2: { value: 'laborum occaecat aliqua occaecat minim mollit qui laborum ea nisi' },
-      },
-      {
-        accessionId: '95dc5557-a467-4bf2-bd2a-c014e1610520',
-        col1: { value: 'velit nisi reprehenderit anim sit culpa sit laborum incididunt laborum' },
-        col2: { value: 'velit aliquip sit ad veniam non tempor ea veniam quis' },
-      },
-      {
-        accessionId: 'bd08daca-43b1-45bf-8c20-828bf166100a',
-        col1: { value: 'laborum eu eiusmod ea ea proident nulla sunt anim deserunt' },
-        col2: { value: 'eu consectetur in sit dolore adipisicing qui dolore consequat ipsum' },
-      },
-      {
-        accessionId: '15f81734-5271-4783-8f2e-749056aae537',
-        col1: { value: 'elit dolore veniam laborum ipsum labore amet enim nostrud ut' },
-        col2: { value: 'Lorem mollit ex sit ut aute aliquip exercitation sunt incididunt' },
-      },
-      {
-        accessionId: '13437acf-75d5-4a6d-9e48-f9949c716726',
-        col1: {
-          value: 'ipsum commodo laboris cupidatat excepteur incididunt quis qui Lorem cillum',
-        },
-        col2: { value: 'incididunt fugiat officia elit proident Lorem nisi non in cupidatat' },
-      },
-      {
-        accessionId: 'd26dc413-25d3-46e5-b5df-6a9bbbfca7ea',
-        col1: { value: 'labore ad pariatur ex quis in est aute ipsum veniam' },
-        col2: {
-          value: 'non mollit exercitation enim incididunt magna mollit magna voluptate aliquip',
-        },
-      },
-      {
-        accessionId: '85e66729-8635-4142-a316-263243540fff',
-        col1: { value: 'laboris culpa ad officia aliquip deserunt elit commodo ex ad' },
-        col2: { value: 'consectetur ex do veniam velit fugiat fugiat magna laborum fugiat' },
-      },
-      {
-        accessionId: '88b590b8-8093-4aca-8e65-24f5f809bfc7',
-        col1: { value: 'sit cupidatat pariatur anim fugiat elit ullamco dolore Lorem tempor' },
-        col2: { value: 'pariatur non duis Lorem pariatur eiusmod nostrud anim aliqua aliqua' },
-      },
-      {
-        accessionId: '2867b132-4f7a-4cb2-ac5c-4c0e44eebf26',
-        col1: { value: 'sint amet sint incididunt ea voluptate dolore enim incididunt minim' },
-        col2: {
-          value:
-            'officia tempor excepteur eiusmod voluptate exercitation minim adipisicing sint reprehenderit',
-        },
-      },
-    ];
+    console.log(data.length);
     return (
-      <div style={{ height: 2000 }}>
+      <div style={{ height: 1000 }}>
         <InfiniteDataTable
           selectable={selectable}
           selected={selected}
--- src/components/infinite-data-table.jsx
+++ src/components/infinite-data-table.jsx
@@ -1,33 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
+  Column,
+  Table,
+  MultiGrid,
   CellMeasurer,
   CellMeasurerCache,
-  MultiGrid,
   AutoSizer,
   InfiniteLoader,
 } from 'react-virtualized';
+import v1 from 'uuid';
+import 'react-virtualized/styles.css'; // only needs to be imported once
+import '../styles/components/infinite-data-table.scss';
 
 const defaultWidth = 200;
 const resultsTableRowIndex = {
   label: '#',
   name: 'resultsTableRowIndex',
   render: (row, index) => index,
+  width: 100,
 };
 
 class InfiniteDataTable extends Component {
   cache = new CellMeasurerCache({
     fixedWidth: true,
-    defaultHeight: 60,
-    // minHeight: 60,
+    defaultHeight: 30,
+    minHeight: 30,
   });
 
   constructor(props) {
     super(props);
     this.handleHeaderKeyPress = this.handleHeaderKeyPress.bind(this);
-    this.getColumnWidth = this.getColumnWidth.bind(this);
-    this.cellRenderer = this.cellRenderer.bind(this);
+    // this.getColumnWidth = this.getColumnWidth.bind(this);
+    // this.cellRenderer = this.cellRenderer.bind(this);
     this.isRowLoaded = this.isRowLoaded.bind(this);
+    this.setTableRef = this.setTableRef.bind(this);
   }
 
   static getRowClassName(index, selected = false) {
@@ -44,6 +51,102 @@ class InfiniteDataTable extends Component {
     return 'table-header-sortable';
   }
 
+  static getNumberColumn() {
+    return (
+      <Column
+        key={v1()}
+        label="#"
+        dataKey="index"
+        width={50}
+        minWidth={50}
+        cellDataGetter={columnData => columnData}
+        cellRenderer={({ rowIndex }) => rowIndex + 1}
+      />
+    );
+  }
+
+  componentDidUpdate() {
+    this.cache.clearAll();
+    const { data } = this.props;
+    for (let i = 0; i <= data.length; i += 1) {
+      this.table.recomputeRowHeights(i);
+    }
+    this.cache.clearAll();
+  }
+
+  getTableWidth() {
+    let { columns } = this.props;
+    const { showRowNumbers } = this.props;
+    if (showRowNumbers) {
+      columns = [resultsTableRowIndex, ...columns];
+    }
+    const width = columns.reduce(
+      (totalWidth, column) => totalWidth + column.width || defaultWidth,
+      0,
+    );
+    return width;
+  }
+
+  getColumns() {
+    const { columns, showRowNumbers } = this.props;
+    const columnNodes = columns.map((column) => {
+      let render;
+      if (column.render) {
+        render = row => (row ? column.render(row) : <span style={{ fontSize: 100 }}>loading...</span>);
+      } else {
+        render = () => <div className="warning">{`${column.label} has no render method`}</div>;
+      }
+
+      // const getContent = measure => {
+      //   // store callback for later use
+      //   this.measureCallbacks[index] = measure;
+      //   return content;
+      // };
+
+      return (
+        <Column
+          key={v1()}
+          label={column.label}
+          dataKey={column.name}
+          cellDataGetter={rowData => rowData}
+          width={column.width}
+          minWidth={column.width}
+          // cellRenderer={({
+          //   rowData, parent, dataKey, rowIndex, style,
+          // }) => render(rowData)}
+          cellRenderer={({
+            rowData, parent, dataKey, rowIndex,
+          }) => (
+            <CellMeasurer
+              cache={this.cache}
+              columnIndex={0}
+              key={dataKey}
+              parent={parent}
+              rowIndex={rowIndex}
+            >
+              <div
+                style={{
+                  overflow: 'normal',
+                  whiteSpace: 'normal',
+                }}
+              >
+                {render(rowData)}
+              </div>
+            </CellMeasurer>
+          )}
+        />
+      );
+      // return {
+      //   label: columnName,
+      //   name: columnName,
+      //   render,
+      //   sortable: columnName in SortableColumns,
+      //   sorted: columnName === sort.column ? sort.direction : false,
+      // };
+    });
+    return showRowNumbers ? [InfiniteDataTable.getNumberColumn(), ...columnNodes] : columnNodes;
+  }
+
   handleHeaderKeyPress(event, columnName) {
     const { onHeaderClick } = this.props;
     if (event.key === 'Enter') {
@@ -51,72 +154,73 @@ class InfiniteDataTable extends Component {
     }
   }
 
-  cellRenderer({
-    columnIndex, rowIndex, key, parent, style,
-  }) {
-    let { columns } = this.props;
-    const {
-      data, fixedRowCount, fixedColumnCount, showHeader, showRowNumbers,
-    } = this.props;
-    let className = '';
-    let cellContent;
+  // cellRenderer({
+  //   columnIndex, rowIndex, key, parent, style,
+  // }) {
+  //   let { columns } = this.props;
+  //   const {
+  //     data, fixedRowCount, fixedColumnCount, showHeader, showRowNumbers,
+  //   } = this.props;
+  //   let className = '';
+  //   let cellContent;
 
-    if (showRowNumbers) {
-      columns = [resultsTableRowIndex, ...columns];
-    }
+  //   if (showRowNumbers) {
+  //     columns = [resultsTableRowIndex, ...columns];
+  //   }
 
-    if (fixedColumnCount && columnIndex < fixedColumnCount) {
-      className += ' frozen-column';
-    }
+  //   if (fixedColumnCount && columnIndex < fixedColumnCount) {
+  //     className += ' frozen-column';
+  //   }
 
-    if (fixedRowCount && rowIndex < fixedRowCount) {
-      className += ' frozen-row';
-    }
+  //   if (fixedRowCount && rowIndex < fixedRowCount) {
+  //     className += ' frozen-row';
+  //   }
 
-    const ri = rowIndex - Number(showHeader);
-    const column = columns[columnIndex];
-    if (ri === -1) {
-      cellContent = 'label' in column ? <b>{column.label}</b> : `${columnIndex} has no label`;
-    } else {
-      const row = data[ri];
-      if (!className.includes('frozen-column')) {
-        className += ri % 2 ? ' table-row-odd' : ' table-row-even';
-      }
-      if (!row) {
-        cellContent = 'loading...';
-      } else if ('render' in column) {
-        cellContent = column.render(row, rowIndex);
-      } else {
-        cellContent = <div className="warning">{`${columnIndex} has no render method`}</div>;
-      }
-    }
-    // console.log(style);
-    return (
-      <CellMeasurer
-        cache={this.cache}
-        columnIndex={columnIndex}
-        key={key}
-        parent={parent}
-        rowIndex={ri}
-      >
-        <div style={style}>{cellContent}</div>
-      </CellMeasurer>
-    );
-  }
-
-  getColumnWidth({ index }) {
-    let { columns } = this.props;
-    const { showRowNumbers } = this.props;
-    if (showRowNumbers) {
-      columns = [resultsTableRowIndex, ...columns];
-    }
-    const column = columns[index];
-    return 'width' in column ? column.width : defaultWidth;
-  }
+  //   const ri = rowIndex - Number(showHeader);
+  //   const column = columns[columnIndex];
+  //   if (ri === -1) {
+  //     cellContent = 'label' in column ? <b>{column.label}</b> : `${columnIndex} has no label`;
+  //   } else {
+  //     const row = data[ri];
+  //     if (!className.includes('frozen-column')) {
+  //       className += ri % 2 ? ' table-row-odd' : ' table-row-even';
+  //     }
+  //     if (!row) {
+  //       cellContent = <span style={{ fontSize: 100 }}>loading...</span>;
+  //     } else if ('render' in column) {
+  //       cellContent = column.render(row, rowIndex);
+  //     } else {
+  //       cellContent = (
+  //         <div style={{ fontSize: 100 }} className="warning">
+  //           {`${columnIndex} has no render method`}
+  //         </div>
+  //       );
+  //     }
+  //   }
+  //   return (
+  //     <CellMeasurer
+  //       cache={this.cache}
+  //       columnIndex={columnIndex}
+  //       key={key}
+  //       parent={parent}
+  //       rowIndex={ri}
+  //     >
+  //       <div
+  //         style={{
+  //           ...style,
+  //           minWidth: column.width,
+  //           // paddingBottom: 30,
+  //           // paddingTop: 30,
+  //         }}
+  //       >
+  //         {cellContent}
+  //       </div>
+  //     </CellMeasurer>
+  //   );
+  // }
 
   isRowLoaded({ index }) {
-    const { data, showRowNumbers } = this.props;
-    console.log(index, !!data[index]);
+    const { data } = this.props;
     return !!data[index];
   }
 
@@ -131,17 +235,14 @@ class InfiniteDataTable extends Component {
       idKey,
       onLoadMoreRows,
       showHeader,
-      fixedColumnCount,
-      fixedRowCount,
       totalNumberRows,
       numberResultsPerRequest,
       showRowNumbers,
     } = this.props;
-    const rowCount = Math.min(
-      totalNumberRows + Number(showHeader),
-      data.length + numberResultsPerRequest - 1 + Number(showHeader),
-    );
-    console.log(rowCount, totalNumberRows, data.length);
+    const rowCount = Math.min(totalNumberRows, data.length + numberResultsPerRequest);
+    console.log(this.cache);
+    console.log(this.table);
+
     return (
       <InfiniteLoader
         loadMoreRows={onLoadMoreRows}
@@ -150,33 +251,28 @@ class InfiniteDataTable extends Component {
         threshold={1}
       >
         {({ onRowsRendered, registerChild }) => (
-          <AutoSizer>
-            {({ width, height }) => (
-              <MultiGrid
-                cellRenderer={this.cellRenderer}
-                columnCount={columns.length + Number(showRowNumbers)}
-                width={width}
-                height={height}
-                // columnWidth={this.getColumnWidth}
-                columnWidth={300}
-                rowHeight={this.cache.rowHeight}
-                deferredMeasurementCache={this.cache}
-                // fixedColumnCount={fixedColumnCount}
-                // fixedRowCount={fixedRowCount}
-                ref={registerChild}
-                rowCount={rowCount}
-                scrollToRow={data.length <= numberResultsPerRequest ? 0 : undefined}
-                onSectionRendered={({ rowStartIndex, rowStopIndex }) => onRowsRendered({
-                  startIndex: rowStartIndex,
-                  stopIndex: rowStopIndex,
-                })
-                }
-              />
-            )}
-          </AutoSizer>
+          <Table
+            width={this.getTableWidth()}
+            height={500}
+            headerHeight={150}
+            deferredMeasurementCache={this.cache}
+            rowHeight={this.cache.rowHeight}
+            ref={ref => this.setTableRef(ref, registerChild)}
+            onRowsRendered={onRowsRendered}
+            rowCount={rowCount}
+            rowGetter={({ index }) => data[index]}
+            scrollToIndex={data.length <= numberResultsPerRequest ? 0 : undefined}
+          >
+            {this.getColumns()}
+          </Table>
         )}
       </InfiniteLoader>
     );
+  }
+
+  setTableRef(ref, registerChild) {
+    this.table = ref;
+    registerChild(ref);
   }
 }
 
