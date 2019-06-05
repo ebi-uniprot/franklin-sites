cbb81c554d15360b8fecaa354efc619283a68a6b
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-06-05 11:17:33 +0100

    Add implicit-arrow-linebreak: off to eslintrc.

--- src/app/common/lipsum.jsx
+++ src/app/common/lipsum.jsx
@@ -1,22 +1,25 @@
 import { loremIpsum } from 'lorem-ipsum';
 import v1 from 'uuid';
 
-export const getLipsumSentence = () => loremIpsum({
-  sentenceLowerBound: 2,
-  sentenceUpperBound: 30,
-});
-
-export const fillArray = (numberElements, func) => Array(numberElements)
-  .fill(null)
-  .map(func);
-
-export const getLipsumObjectArray = ({ numberElements, keys, idKey }) => fillArray(numberElements, () => {
-  const dataPoint = { [idKey]: v1() };
-  keys.forEach((key) => {
-    const text = getLipsumSentence();
-    dataPoint[key] = text;
+export const getLipsumSentence = () =>
+  loremIpsum({
+    sentenceLowerBound: 2,
+    sentenceUpperBound: 30,
   });
-  return dataPoint;
-});
+
+export const fillArray = (numberElements, func) =>
+  Array(numberElements)
+    .fill(null)
+    .map(func);
+
+export const getLipsumObjectArray = ({ numberElements, keys, idKey }) =>
+  fillArray(numberElements, () => {
+    const dataPoint = { [idKey]: v1() };
+    keys.forEach((key) => {
+      const text = getLipsumSentence();
+      dataPoint[key] = text;
+    });
+    return dataPoint;
+  });
 
 export default getLipsumObjectArray;
