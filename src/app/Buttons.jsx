9777cba5c75098130e03b9dbd86376c55ab9d58c
Xavier Watkins <xavier.watkins@gmail.com>
2019-06-07 13:02:54 +0100

    Add different button styles, and new icons

---
+++ src/app/Buttons.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './styles/atoms.scss';
+import { DownloadIcon } from '../components';
+
+const Links = () => (
+  <section className="atoms-section" id="buttons">
+    <h1>Buttons</h1>
+    <section>
+      <section>
+        <button className="button" type="button">
+          Primary
+        </button>
+        <p> Use primary buttons for main actions.</p>
+      </section>
+      <section>
+        <button className="button secondary" type="button">
+          Secondary
+        </button>
+        <p>This hollowed style of buttons is more suited for secondary actions such as Cancel.</p>
+      </section>
+      <section>
+        <button className="button link-button" type="button">
+          Link button
+        </button>
+        <p>
+          This style of buttons is more discrete and works great for groups of actions or inline
+          text.
+        </p>
+      </section>
+    </section>
+    <section>
+      <h4>Button groups</h4>
+      <p>Button groups are used to display multiple buttons in a row together.</p>
+      <div className="button-group">
+        <button className="button" type="button">
+          One
+        </button>
+        <button className="button" type="button">
+          Two
+        </button>
+        <button className="button" type="button">
+          Three
+        </button>
+      </div>
+      <div className="button-group">
+        <button className="button link-button" type="button">
+          One
+        </button>
+        <button className="button link-button" type="button">
+          Two
+        </button>
+        <button className="button link-button" type="button">
+          Three
+        </button>
+      </div>
+    </section>
+    <section>
+      <h4>Disabled buttons</h4>
+      <p>
+        Use the class
+        {' '}
+        <em>.disabled</em>
+      </p>
+      <div className="button-group">
+        <button className="button disabled" type="button">
+          One
+        </button>
+        <button className="button secondary disabled" type="button">
+          Two
+        </button>
+        <button className="button link-button disabled" type="button">
+          Three
+        </button>
+      </div>
+    </section>
+    <section>
+      <h4>Icon buttons</h4>
+      <p>You can use icon components directly within an icon.</p>
+      <div className="button-group">
+        <button className="button" type="button">
+          <DownloadIcon />
+          Download
+        </button>
+        <button className="button secondary" type="button">
+          <DownloadIcon />
+          Download
+        </button>
+        <button className="button link-button" type="button">
+          <DownloadIcon />
+          Download
+        </button>
+      </div>
+    </section>
+  </section>
+);
+
+export default Links;
--- src/app/Icons.jsx
+++ src/app/Icons.jsx
@@ -1,50 +1,70 @@
 import React from 'react';
-import Obsolete from '../svg/obsolete.svg';
-import SwissProt from '../svg/swissprot.svg';
-import Trembl from '../svg/trembl.svg';
-import ChevronDown from '../svg/chevron-down.svg';
-import ChevronUp from '../svg/chevron-up.svg';
-import Tag from '../svg/tag.svg';
-import ExternalLink from '../svg/external-link.svg';
 import './styles/atoms.scss';
+import {
+  SwissProtIcon,
+  TremblIcon,
+  ObsoleteIcon,
+  TagIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+  ExternalLinkIcon,
+  DownloadIcon,
+  BasketIcon,
+  StatisticsIcon,
+} from '../components';
 
-const size = 50;
+const size = 30;
 
 const iconDefinition = [
   {
     name: 'swissprot.svg',
     description: 'SwissProt reviewed',
-    icon: <SwissProt width={size} height={size} />,
+    icon: <SwissProtIcon width={size} height={size} />,
   },
   {
     name: 'trembl.svg',
     description: 'Trembl unreviewed',
-    icon: <Trembl width={size} height={size} />,
+    icon: <TremblIcon width={size} height={size} />,
   },
   {
     name: 'obsolete.svg',
     description: 'Obsolete entry',
-    icon: <Obsolete width={size} height={size} />,
+    icon: <ObsoleteIcon width={size} height={size} />,
   },
   {
     name: 'tag.svg',
     description: 'Tag',
-    icon: <Tag width={size} height={size} />,
+    icon: <TagIcon width={size} height={size} />,
   },
   {
     name: 'chevron-up.svg',
     description: 'Chevron Up',
-    icon: <ChevronUp width={size} height={size} />,
+    icon: <ChevronUpIcon width={size} height={size} />,
   },
   {
     name: 'chevron-down.svg',
     description: 'Chevron Down',
-    icon: <ChevronDown width={size} height={size} />,
+    icon: <ChevronDownIcon width={size} height={size} />,
   },
   {
     name: 'external-link.svg',
     description: 'External Link',
-    icon: <ExternalLink width={size} height={size} />,
+    icon: <ExternalLinkIcon width={size} height={size} />,
+  },
+  {
+    name: 'download.svg',
+    description: 'Download',
+    icon: <DownloadIcon width={size} height={size} />,
+  },
+  {
+    name: 'basket.svg',
+    description: 'Basket',
+    icon: <BasketIcon width={size} height={size} />,
+  },
+  {
+    name: 'statistics.svg',
+    description: 'Statistics',
+    icon: <StatisticsIcon width={size} height={size} />,
   },
 ];
 
@@ -54,8 +74,10 @@ const Icons = () => (
     <div className="icon-list">
       {iconDefinition.map(d => (
         <div className="icon-container" key={d.name}>
-          <h5>{d.description}</h5>
           {d.icon}
+          <div>
+            <small>{d.description}</small>
+          </div>
         </div>
       ))}
     </div>
