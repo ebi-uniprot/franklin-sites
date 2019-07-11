2f5e21d050b78669330902cfad58165e4c6c0ab5
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-07-11 16:21:13 +0100

    Export ExpandableList from package.

--- src/components/index.js
+++ src/components/index.js
@@ -16,6 +16,7 @@ export { default as Sequence } from './sequence';
 export { default as ExternalLink } from './external-link';
 export { default as InPageNav } from './in-page-nav';
 export { default as Loader } from './loader';
+export { default as ExpandableList } from './expandable-list';
 
 export { default as ObsoleteIcon } from '../svg/obsolete.svg';
 export { default as SwissProtIcon } from '../svg/swissprot.svg';
