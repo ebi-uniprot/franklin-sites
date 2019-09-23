7ad71f4dbb41954936d6152770665aaca85a6e25
Xavier Watkins <xavier.watkins@gmail.com>
2019-09-23 17:20:40 +0100

    Add functionality for dropdown in header nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../styles/components/header.scss';
+import '../styles/components/dropdown.scss';
 
 const Header = ({
   logo, links, search, isNegative,
@@ -11,11 +12,33 @@ const Header = ({
       <Link to="/">{logo}</Link>
     </div>
     <ul className="header__navigation">
-      {links.map(link => (
-        <li key={link.label}>
-          <Link to={link.path}>{link.label}</Link>
-        </li>
-      ))}
+      {links.map(link =>
+        (link.links ? (
+          <li className="dropdown-container dropdown-container--hover" key={link.label}>
+            <span
+              className={
+                isNegative
+                  ? 'dropdown-container__trigger dropdown-container__trigger--negative'
+                  : 'dropdown-container__trigger'
+              }
+            >
+              {link.label}
+            </span>
+            <div className="dropdown-menu">
+              <ul>
+                {link.links.map(link2 => (
+                  <li key={link2.label}>
+                    <Link to={link2.path}>{link2.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        ) : (
+          <li key={link.label}>
+            <Link to={link.path}>{link.label}</Link>
+          </li>
+        )))}
     </ul>
     <div className="header__search">{search}</div>
   </div>
