536cbb54b4ab8ba9a2418779d496d182dd013088
Daniel Rice <dlrice@ebi.ac.uk>
2019-10-09 11:32:48 +0100

    main-search: add data-testid's; convert tests to react-testing-library; create snapshot.

--- src/components/__tests__/main-search.spec.jsx
+++ src/components/__tests__/main-search.spec.jsx
@@ -1,39 +1,32 @@
 import React from 'react';
-import Enzyme, { mount, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import { render, fireEvent } from '@testing-library/react';
 import MainSearch from '../main-search';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('MainSearch component', () => {
   const handleSubmit = jest.fn();
   const handleChange = jest.fn();
+  const props = { onChange: handleChange, onSubmit: handleSubmit };
+  test('should render', () => {
+    const { asFragment } = render(<MainSearch {...props} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
   test('should submit the search term', () => {
-    const wrapper = mount(<MainSearch onChange={handleChange} onSubmit={handleSubmit} />);
-    wrapper.find('[type="submit"]').simulate('submit');
+    const { getByTestId } = render(<MainSearch {...props} />);
+    fireEvent.submit(getByTestId('main-search-form'));
     expect(handleSubmit).toHaveBeenCalled();
   });
 
   test('should detect the search term', () => {
-    const wrapper = mount(<MainSearch onChange={handleChange} onSubmit={handleSubmit} />);
-    wrapper
-      .find('[type="text"]')
-      .first()
-      .simulate('change', { target: { value: 'foo' } });
+    const { getByTestId } = render(<MainSearch {...props} />);
+    fireEvent.change(getByTestId('main-search-input'), {
+      target: { value: 'foo' },
+    });
     expect(handleChange).toHaveBeenCalledWith('foo');
   });
 
   test('should set searchTerm', () => {
-    const wrapper = shallow(
-      <MainSearch searchTerm="blah" onChange={handleChange} onSubmit={handleSubmit} />,
-    );
-    expect(
-      wrapper
-        .find('[type="text"]')
-        .first()
-        .props().value,
-    ).toBe('blah');
+    const { getByTestId } = render(<MainSearch {...props} searchTerm="blah" />);
+    expect(getByTestId('main-search-input').value).toBe('blah');
   });
 });
