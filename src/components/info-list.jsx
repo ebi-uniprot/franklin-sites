118612b893f66d3d99ab69e3f4082a760537d291
Xavier Watkins <xavier.watkins@gmail.com>
2019-10-15 10:14:51 +0100

    Add flat option to infolist

--- src/components/info-list.jsx
+++ src/components/info-list.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import '../styles/components/info-list.scss';
 
-const InfoList = ({ infoData, columns }) => (
-  <div className={`info-list${columns ? ' info-list--columns' : ''}`}>
-    {infoData.map(
-      // Only draw if there is content
-      item =>
-        item.content && (
-          <div className="info-list__item" key={item.title}>
-            <div className="info-list__item__title">{item.title}</div>
-            <div className="info-list__item__content">{item.content}</div>
-          </div>
-        ),
-    )}
-  </div>
-);
-
+const InfoList = ({ infoData, columns, isFlat }) =>
+  (!isFlat ? (
+    <div className={`info-list${columns ? ' info-list--columns' : ''}`}>
+      {infoData.map(
+        // Only draw if there is content
+        item =>
+          item.content && (
+            <div className="info-list__item" key={item.title}>
+              <div className="info-list__item__title">{item.title}</div>
+              <div className="info-list__item__content">{item.content}</div>
+            </div>
+          ),
+      )}
+    </div>
+  ) : (
+    <Fragment>
+      {infoData.map((item, index) => (
+        <div key={item.title}>{index === 0 ? <strong>{item.content}</strong> : item.content}</div>
+      ))}
+    </Fragment>
+  ));
 InfoList.propTypes = {
   /**
    * An array of objects each containing 'title' and 'content'
@@ -33,10 +39,16 @@ InfoList.propTypes = {
    * columns on medium to large screens or not.
    */
   columns: PropTypes.bool,
+  /**
+   * A boolean indicating whether the component should render
+   * as a flat list
+   */
+  isFlat: PropTypes.bool,
 };
 
 InfoList.defaultProps = {
   columns: false,
+  isFlat: false,
 };
 
 export default InfoList;
