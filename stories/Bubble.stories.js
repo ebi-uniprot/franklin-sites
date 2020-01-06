fbfa89ff56962251732e7b7a5255e6ddad9875b9
Xavier Watkins <xavier.watkins@gmail.com>
2020-01-06 15:57:26 +0000

    More stories

---
+++ stories/Bubble.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Bubble from '../src/components/bubble';
+
+export default {
+  title: 'Atoms|Bubble',
+  parameters: {
+    purposeFunction: {
+      purpose: 'Highlight numbers.',
+      function: 'If there is more than 100 items the bubble will show 99+.',
+    },
+  },
+};
+
+export const smallBubble = () => <Bubble size="small" value={56} />;
+export const regularBubble = () => <Bubble value={1000} />;
+export const largeBubble = () => (
+  <Bubble size="large" value={22.5} colourClass="colour-proteomes" />
+);
---
+++ stories/Doughnut.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import DoughnutChart from '../src/components/doughnut-chart';
+
+export default {
+  title: 'Atoms|Doughnut',
+  parameters: {
+    purposeFunction: {
+      purpose: 'Highlight a ratio.',
+      function:
+        'Used to represent numbers which represent a quantity relative to a total. By default shown as percentages but custom text can be shown instead',
+    },
+  },
+};
+
+export const smallDoughnut = () => (
+  <DoughnutChart percent={90} size="small">
+    9
+  </DoughnutChart>
+);
+export const regularDoughnut = () => (
+  <DoughnutChart
+    percent={60}
+    borderWidth={10}
+    colorClass="colour-uniref"
+    bgColorClass="colour-seashell-grey"
+  >
+    3/5
+  </DoughnutChart>
+);
+export const largeDoughnut = () => <DoughnutChart percent={20} size="large" />;
