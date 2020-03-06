612e5fc31908edfa029a8eaa9251f0efa7b75209
Daniel Rice <dlrice@ebi.ac.uk>
2020-03-06 14:55:29 +0000

    Convert Dropdown to functional component.

--- src/components/dropdown-button.jsx
+++ src/components/dropdown-button.jsx
@@ -1,82 +1,58 @@
-import React, { Component, createRef } from 'react';
+import React, { createRef, useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/components/dropdown.scss';
 
-class DropdownButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMenu: false,
-      buttonHeight: 0,
+const DropdownButton = ({ children, label, className }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [buttonHeight, setButtonHeight] = useState(0);
+  const buttonRef = createRef();
+  const containerRef = createRef();
+
+  const handleClick = useCallback(
+    e => {
+      if (containerRef.current && containerRef.current.contains(e.target)) {
+        return;
+      }
+      setShowMenu(false);
+    },
+    [containerRef]
+  );
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      const { height } = buttonRef.current.getBoundingClientRect();
+      setButtonHeight(height);
+    }
+    if (containerRef.current) {
+      document.addEventListener('mousedown', handleClick, false);
+    }
+    const cleanUp = () => {
+      document.removeEventListener('mousedown', handleClick);
     };
-    this.buttonRef = createRef();
-    this.handleClick = this.handleClick.bind(this);
-    this.setContainerRef = this.setContainerRef.bind(this);
-    this.documentMousedownEventListening = false;
-  }
+    return cleanUp;
+  }, [buttonRef, containerRef, handleClick]);
 
-  componentDidMount() {
-    if (this.buttonRef.current) {
-      const { height } = this.buttonRef.current.getBoundingClientRect();
-      this.setState({ buttonHeight: height });
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.documentMousedownEventListening) {
-      document.removeEventListener('mousedown', this.handleClick);
-    }
-  }
-
-  setContainerRef(ref) {
-    this.containerRef = ref;
-  }
-
-  handleClick(e) {
-    if (this.containerRef.contains(e.target)) {
-      return;
-    }
-    this.setState({ showMenu: false });
-  }
-
-  toggleDropdown() {
-    if (!this.documentMousedownEventListening) {
-      document.addEventListener('mousedown', this.handleClick, false);
-      this.documentMousedownEventListening = true;
-    }
-    const { showMenu } = this.state;
-    this.setState({ showMenu: !showMenu });
-  }
-
-  close() {
-    this.setState({ showMenu: false });
-  }
-
-  render() {
-    const { children, label, className } = this.props;
-    const { showMenu, buttonHeight } = this.state;
-    return (
-      <div className="dropdown-container" ref={this.setContainerRef}>
-        <button
-          type="button"
-          className={`button dropdown ${className}`}
-          onClick={() => this.toggleDropdown()}
-          ref={this.buttonRef}
-        >
-          {label}
-        </button>
-        <div
-          className={
-            showMenu ? 'dropdown-menu dropdown-menu-open' : 'dropdown-menu'
-          }
-          style={{ top: buttonHeight }}
-        >
-          {children}
-        </div>
+  return (
+    <div className="dropdown-container" ref={containerRef}>
+      <button
+        type="button"
+        className={`button dropdown ${className}`}
+        onClick={() => setShowMenu(!showMenu)}
+        ref={buttonRef}
+      >
+        {label}
+      </button>
+      <div
+        className={
+          showMenu ? 'dropdown-menu dropdown-menu-open' : 'dropdown-menu'
+        }
+        style={{ top: buttonHeight }}
+      >
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 DropdownButton.propTypes = {
   /**
