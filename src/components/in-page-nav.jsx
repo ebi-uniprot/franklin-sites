43e35e734a2b68e19e8db38d937f2ef0ac485568
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-05-07 17:11:27 +0100

    changes in styling and behaviour

--- src/components/in-page-nav.jsx
+++ src/components/in-page-nav.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, useHistory, useRouteMatch } from 'react-router-dom';
 import '../styles/components/in-page-nav.scss';
@@ -6,6 +6,10 @@ import '../styles/components/in-page-nav.scss';
 const InPageNav = ({ sections, path, slug }) => {
   const history = useHistory();
   const match = useRouteMatch(path);
+
+  const elementVisibility = useRef();
+  const clickFlag = useRef(false);
+  const marker = useRef();
 
   const sectionName = match.params[slug];
 
@@ -15,15 +19,28 @@ const InPageNav = ({ sections, path, slug }) => {
     const elements = sections
       .map(section => document.querySelector(`[data-location="${section.id}"]`))
       .filter(Boolean);
+    // add them to a map keeping there visibility state
+    elementVisibility.current = new Map(
+      elements.map(element => [element, true])
+    );
 
     const replaceSlugRE = new RegExp(`:${slug}`);
     // Intersection Observer to watch when sections appear/disappear
     const io = new IntersectionObserver(entries => {
+      // update the visibility map
+      entries.forEach(entry =>
+        elementVisibility.current.set(entry.target, entry.isIntersecting)
+      );
+      // if we are still withing a small time after a click, we ignore
+      if (clickFlag.current) return;
       // find the first section visible (even partially)
-      const visibleEntry = entries.find(entry => entry.isIntersecting);
-      if (!visibleEntry) return;
+      // eslint-disable-next-line no-restricted-syntax
+      const [firstVisible] =
+        Array.from(elementVisibility.current).find(([, visible]) => visible) ||
+        [];
+      if (!firstVisible) return; // 🤷🏽‍♂️
       // get its location from "data-location" attribute
-      const { location } = visibleEntry.target.dataset;
+      const { location } = firstVisible.dataset;
       // calculate what would be the new path
       const newPath = path.replace(replaceSlugRE, location);
       // if it's the same than the current one, don't do anything
@@ -37,9 +54,9 @@ const InPageNav = ({ sections, path, slug }) => {
     return () => elements.forEach(element => io.unobserve(element));
   }, [sections, path, slug, history]);
 
-  // effect to connect user changes to history to elements displaying
+  // effect to connect user changes to history to sections displaying
   useEffect(() => {
-    // find that target element
+    // find that scroll target section
     const element = document.querySelector(
       `[data-location="${decodeURI(sectionName)}"]`
     );
@@ -50,11 +67,51 @@ const InPageNav = ({ sections, path, slug }) => {
     element.scrollIntoView({ behavior: 'smooth' });
   }, [sectionName]);
 
+  useEffect(() => {
+    if (!(marker.current && marker.current.animate)) return;
+
+    const target = marker.current.parentElement.querySelector('.active');
+    if (!target) return;
+
+    // get measurements
+    const containerRect = marker.current.parentElement.getBoundingClientRect();
+    const targetRect = target.getBoundingClientRect();
+
+    marker.current.style.display = 'block';
+    marker.current.animate(
+      {
+        transform: [
+          `translateY(${targetRect.y - containerRect.y}px) scaleY(${
+            targetRect.height
+          })`,
+        ],
+      },
+      {
+        duration: 500,
+        easing: 'cubic-bezier(.5,0,.35,1.25)',
+        fill: 'both',
+      }
+    );
+  }, [history.location.pathname]);
+
+  // set a flag to ignore scroll-related events for 500ms
+  const handleClick = useCallback(() => {
+    clickFlag.current = true;
+    setTimeout(() => {
+      clickFlag.current = false;
+    }, 500);
+  }, []);
+
   return (
     <ul className="in-page-nav">
+      <div ref={marker} className="marker" />
       {sections.map(section => (
         <li key={section.label} className={section.disabled && 'disabled'}>
-          <NavLink to={`${section.id}`} activeClassName="active">
+          <NavLink
+            to={`${section.id}`}
+            activeClassName="active"
+            onClick={handleClick}
+          >
             {section.label}
           </NavLink>
         </li>
