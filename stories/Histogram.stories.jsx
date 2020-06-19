1cf57652dd1a148e3960fd25e72fa3e6a3881f82
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-06-19 15:30:10 +0200

    add example with changing subset of values

--- stories/Histogram.stories.jsx
+++ stories/Histogram.stories.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import { Histogram } from '../src/components';
+
 import {
   getUniformSample,
   getGaussianSample,
@@ -19,5 +21,36 @@ const nValues = 1000;
 const gaussianSample = getGaussianSample(0, 1, nValues);
 const uniformSample = getUniformSample(-10, 10, nValues);
 
+const gaussianMin = Math.min(...gaussianSample);
+const gaussianMax = Math.max(...gaussianSample);
+
+const randomFilter = () => Math.random() > 0.5;
+
+const ChangingGaussianComponent = () => {
+  const interval = useRef();
+
+  // eslint-disable-next-line no-shadow
+  const [filteredSample, setFilteredSample] = useState(
+    gaussianSample.filter(randomFilter)
+  );
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      setFilteredSample(gaussianSample.filter(randomFilter));
+    }, 3000);
+    return () => clearInterval(interval.current);
+  }, []);
+
+  return (
+    <Histogram
+      values={filteredSample}
+      nBins={20}
+      min={gaussianMin}
+      max={gaussianMax}
+    />
+  );
+};
+
 export const Gaussian = () => <Histogram values={gaussianSample} nBins={20} />;
 export const Uniform = () => <Histogram nBins={10} values={uniformSample} />;
+export const ChangingGaussian = () => <ChangingGaussianComponent />;
