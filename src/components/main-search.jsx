57fe43c94fe5d72337516b2687b5b1607e611c9c
Daniel Rice <dlrice@ebi.ac.uk>
2020-11-13 11:32:57 +0000

    Close MainSearch namespace dropdown menu on item click.

--- src/components/main-search.jsx
+++ src/components/main-search.jsx
@@ -17,28 +17,31 @@ const MainSearch = ({
     data-testid="main-search-form"
   >
     {Object.keys(namespaces).length > 0 && (
-      <DropdownButton
-        label={namespaces[selectedNamespace]}
-      >
-        <ul>
-          {Object.keys(namespaces).map(key => (
-            <li key={key}>
-              <button
-                type="button"
-                onClick={() => onNamespaceChange(key)}
-              >
-                {namespaces[key]}
-              </button>
-            </li>
-          ))}
-        </ul>
+      <DropdownButton label={namespaces[selectedNamespace]}>
+        {(setShowMenu) => (
+          <ul>
+            {Object.keys(namespaces).map((key) => (
+              <li key={key}>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setShowMenu(false);
+                    onNamespaceChange(key);
+                  }}
+                >
+                  {namespaces[key]}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </DropdownButton>
     )}
     <input
       type="text"
       className="main-search__input"
       data-testid="main-search-input"
-      onChange={e => onChange(e.target.value)}
+      onChange={(e) => onChange(e.target.value)}
       value={searchTerm}
     />
     <button type="submit" className="button">
