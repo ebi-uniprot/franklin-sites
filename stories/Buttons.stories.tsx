54d9ab6407ad742a6c9b99cf41fdc40aaa98ed5e
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-11-16 16:54:53 +0000

    update stories

--- stories/Buttons.stories.tsx
+++ stories/Buttons.stories.tsx
@@ -32,21 +32,6 @@ export const buttons = () => {
   return (
     <div style={{ '--main-button-color': color } as CSSProperties}>
       <div>
-        <button className="button" type="button">
-          Primary
-        </button>
-      </div>
-      <div>
-        <button className="button secondary" type="button">
-          Secondary
-        </button>
-      </div>
-      <div>
-        <button className="button tertiary" type="button">
-          Tertiary
-        </button>
-      </div>
-      <div>
         <Button disabled={disabled}>Primary</Button>
       </div>
       <div>
@@ -68,28 +53,6 @@ export const buttonGroups = () => {
   const disabled = useDisabled();
   return (
     <div style={{ '--main-button-color': color } as CSSProperties}>
-      <div className="button-group">
-        <button className="button" type="button">
-          One
-        </button>
-        <button className="button" type="button">
-          Two
-        </button>
-        <button className="button" type="button">
-          Three
-        </button>
-      </div>
-      <div className="button-group">
-        <button className="button secondary" type="button">
-          One
-        </button>
-        <button className="button secondary" type="button">
-          Two
-        </button>
-        <button className="button secondary" type="button">
-          Three
-        </button>
-      </div>
       <div className="button-group">
         <button className="button tertiary" type="button">
           One
@@ -137,24 +100,6 @@ export const withIcon = () => {
   const disabled = useDisabled();
   return (
     <div style={{ '--main-button-color': color } as CSSProperties}>
-      <div>
-        <button className="button" type="button">
-          <DownloadIcon />
-          Primary
-        </button>
-      </div>
-      <div>
-        <button className="button secondary" type="button">
-          <DownloadIcon />
-          Secondary
-        </button>
-      </div>
-      <div>
-        <button className="button tertiary" type="button">
-          <DownloadIcon />
-          Tertiary
-        </button>
-      </div>
       <div>
         <Button disabled={disabled}>
           <DownloadIcon />
