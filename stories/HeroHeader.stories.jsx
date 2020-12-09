7012ae7eaf2f0a22efc1d1bc594f3a07a66ca1bf
Xavier Watkins <xavier.watkins@gmail.com>
2020-12-09 13:28:54 +0000

    Change type, fix doc, add knobs

--- stories/HeroHeader.stories.jsx
+++ stories/HeroHeader.stories.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { loremIpsum } from 'lorem-ipsum';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import { HeroHeader } from '../src/components';
+
+const useTitle = () => text('Title', 'Title');
+const useFooter = () =>
+  text('Footer', loremIpsum({ count: 25, units: 'words' }));
 
 export default {
   title: 'Layout/Hero header',
+  decorators: [withKnobs()],
   parameters: {
     purposeFunction: {
       purpose: 'Provide an entry point',
@@ -13,10 +19,7 @@ export default {
 };
 
 export const heroHeader = () => (
-  <HeroHeader
-    title="Title"
-    footer={<span>{loremIpsum({ count: 25, units: 'words' })}</span>}
-  >
+  <HeroHeader title={useTitle()} footer={useFooter()}>
     <input type="text" />
   </HeroHeader>
 );
