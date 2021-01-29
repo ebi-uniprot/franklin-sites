efd7af9e4f2f388c0b4452f46d82182c338217c2
Aurélien Luciani <aurel-l@users.noreply.github.com>
2021-01-29 16:39:35 +0000

    slip switch to TS for Message component in the bundle

---
+++ src/components/message.tsx
@@ -0,0 +1,90 @@
+import { FC, ReactNode, HTMLAttributes, MouseEvent } from 'react';
+import cn from 'classnames';
+
+import WarningIcon from '../svg/warning.svg';
+import WarningTriangleIcon from '../svg/warning-triangle.svg';
+import ErrorIcon from '../svg/error.svg';
+import CloseIcon from '../svg/times.svg';
+
+import '../styles/components/message.scss';
+
+const iconSize = '1.125em';
+
+type Props = {
+  /**
+   * The message level: 'warning', 'failure', 'success', 'info' (default)
+   */
+  level?: 'warning' | 'failure' | 'success' | 'info';
+  /**
+   * The content to appear underneath of the main message
+   */
+  subtitle?: ReactNode;
+  /**
+   * Whether the message can be closed or not
+   */
+  onDismiss?: (event: MouseEvent) => void;
+  /**
+   * To hide the default message icon
+   */
+  noIcon?: boolean;
+  /**
+   * To hide the default box shadow
+   */
+  noShadow?: boolean;
+  /**
+   * To apply any specific styles required for messages shown in
+   * full-page error pages
+   */
+  forFullPage?: boolean;
+};
+
+const Message: FC<Props & HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  level = 'info',
+  subtitle,
+  onDismiss,
+  noIcon,
+  noShadow,
+  forFullPage,
+  className,
+  ...props
+}) => {
+  let maybeIcon = null;
+  if (!noIcon && !forFullPage) {
+    maybeIcon = <WarningIcon width={iconSize} height={iconSize} />;
+    if (level === 'warning') {
+      maybeIcon = <WarningTriangleIcon width={iconSize} height={iconSize} />;
+    } else if (level === 'failure') {
+      maybeIcon = <ErrorIcon width={iconSize} height={iconSize} />;
+    }
+  }
+
+  return (
+    <div
+      className={cn(className, 'message', `message--${level}`, {
+        'message--no-shadow': noShadow || forFullPage,
+        'message--for-full-page': forFullPage,
+      })}
+      role="status"
+      {...props}
+    >
+      <div className="message__side-border" />
+
+      {maybeIcon}
+
+      <section className="message__content">
+        {forFullPage ? <h3>{children}</h3> : <small>{children}</small>}
+      </section>
+
+      {onDismiss && (
+        <button type="button" className="message__dismiss" onClick={onDismiss}>
+          <CloseIcon width="10" height="10" />
+        </button>
+      )}
+
+      {subtitle && <div className="message__subtitle">{subtitle}</div>}
+    </div>
+  );
+};
+
+export default Message;
