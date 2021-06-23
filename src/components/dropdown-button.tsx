50f6aec2f355101fe9d8735caddddacad22360b7
Daniel Rice <dlrice@ebi.ac.uk>
2021-06-23 11:45:53 +0100

    Update comment with jira link.

--- src/components/dropdown-button.tsx
+++ src/components/dropdown-button.tsx
@@ -98,7 +98,10 @@ const DropdownButton = ({
   return (
     <div
       className="dropdown-container"
-      // TODO: uncomment when Safari resolves bug: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#clicking_and_focus
+      // TODO: This code has been commented out as part of https://www.ebi.ac.uk/panda/jira/browse/TRM-25862
+      // Because Safari doesn't focus when clicking on a button: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#clicking_and_focus
+      // a jira has been made https://www.ebi.ac.uk/panda/jira/browse/TRM-26188 to investigate and implement ways to deal with this.
+      // Will leave the code here has a marker for that task.
       // onBlur={(e) =>
       //   setShowMenu(e.currentTarget.contains(e.relatedTarget as Node))
       // }
