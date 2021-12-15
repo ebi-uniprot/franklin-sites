0b8a0a5d2509d55625f738457acd4d0c68dd6f7f
Xavier Watkins <xavier.watkins@gmail.com>
2021-12-15 14:19:15 +0000

    re-define type

--- src/components/hero-container.tsx
+++ src/components/hero-container.tsx
@@ -42,7 +42,7 @@ export const HeroContainer: FC<HeroContainerProps> = ({
     {title &&
       createElement(
         headingLevel,
-        { className: cn(titleClassName, 'hero-container__title', 'medium') },
+        { className: cn(titleClassName, 'hero-container__title', 'big') },
         title
       )}
     {children}
