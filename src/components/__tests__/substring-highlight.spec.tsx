07250b22610f98e3c89152ffba78539dfd497363
Daniel Rice <dlrice@ebi.ac.uk>
2023-03-02 15:58:48 +0000

    Update tests.

--- src/components/__tests__/substring-highlight.spec.tsx
+++ src/components/__tests__/substring-highlight.spec.tsx
@@ -1,17 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import { SubstringHighlight } from '..';
 
+// TODO: Can't get mark by role yet but it should be possible in the future:
+// https://github.com/testing-library/dom-testing-library/issues/1150
+
 describe('SubstringHighlight', () => {
   const string = 'Item 1 / Item 1a';
   it('should highlight substring (case insensitive)', () => {
-    render(
+    const { container } = render(
       <SubstringHighlight substring="item 1">{string}</SubstringHighlight>
     );
     expect(screen.queryByText(string)).not.toBeInTheDocument();
-    expect(screen.getByText('Item 1')).toHaveClass('highlight');
+    const highlight = container.querySelector('mark');
+    expect(highlight).toHaveTextContent('Item 1');
   });
   it('should render string without highlight if no substring found', () => {
-    render(<SubstringHighlight substring="zap">{string}</SubstringHighlight>);
-    expect(screen.getByText(string)).not.toHaveClass('highlight');
+    const { container } = render(
+      <SubstringHighlight substring="zap">{string}</SubstringHighlight>
+    );
+    const highlight = container.querySelector('mark');
+    expect(highlight).toBeNull();
   });
 });
