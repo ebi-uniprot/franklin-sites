180dcda350b0f3d189343e62a80307f7cc976efd
Daniel Rice <dlrice@ebi.ac.uk>
2023-03-09 17:23:21 +0000

    Create BytesNumber story.

---
+++ stories/BytesNumber.stories.tsx
@@ -0,0 +1,13 @@
+import { number } from '@storybook/addon-knobs';
+
+import { BytesNumber } from '../src/components';
+
+export default {
+  title: 'Visualisation/Bytes number',
+};
+
+export const bytesNumber = () => (
+  <BytesNumber decimals={number('decimals', 0)}>
+    {number('bytes', 1024)}
+  </BytesNumber>
+);
