5f7591c7f48baa4cad7f627520a353dfe8d74c38
Daniel Rice <dlrice@ebi.ac.uk>
2023-05-18 09:29:04 +0100

    Copy to clipboard: update tests.

--- src/components/__tests__/copy-to-clipboard.spec.tsx
+++ src/components/__tests__/copy-to-clipboard.spec.tsx
@@ -35,4 +35,19 @@ describe('CopyToClipboard component', () => {
     fireEvent.click(beforeCopyButton);
     await waitFor(() => screen.getByText('Copied it'));
   });
+
+  it('should call onCopy when clicked', async () => {
+    const onCopy = jest.fn();
+    render(
+      <CopyToClipboard
+        textToCopy={toCopy}
+        beforeCopy="Copy this"
+        afterCopy="Copied it"
+        onCopy={onCopy}
+      />
+    );
+    const beforeCopyButton = screen.getByText('Copy this');
+    fireEvent.click(beforeCopyButton);
+    await waitFor(() => expect(onCopy).toHaveBeenCalledWith(toCopy));
+  });
 });
