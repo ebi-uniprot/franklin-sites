d7e09c6fdac784e147d784b75b9124d6a63df52b
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-19 11:41:56 +0000

    Migrate storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../stories/**/*.stories.tsx'],
+
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-knobs',
     '@storybook/addon-actions/register',
     './purposeFunction/register.js',
   ],
+
   staticDirs: ['../assets'],
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,26 @@
-import React from 'react'; // Still needed for deployed storybook to work
-import { addDecorator, addParameters } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import StoryRouter from 'storybook-react-router';
+import React from "react"; // Still needed for deployed storybook to work
+import StoryRouter from "storybook-react-router";
 
-import franklinTheme from './franklin-theme';
+import franklinTheme from "./franklin-theme";
 
-import '../src/styles/index.scss';
+import "../src/styles/index.scss";
 
-addDecorator(withInfo);
-
-addDecorator(StoryRouter());
-
-addDecorator((storyFn) => (
-  <div
-    style={{
-      padding: '2rem 10rem',
-    }}
-  >
-    {storyFn()}
-  </div>
-));
-
-addParameters({
+export const parameters = {
   docs: {
     theme: franklinTheme,
   },
-});
+};
+
+export const decorators = [
+  withInfo,
+  StoryRouter(),
+  (storyFn) => (
+    <div
+      style={{
+        padding: "2rem 10rem",
+      }}
+    >
+      {storyFn()}
+    </div>
+  ),
+];
