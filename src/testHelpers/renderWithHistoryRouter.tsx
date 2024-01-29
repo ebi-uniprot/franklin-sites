58069020a873fc08c0315f980d9e99bcc3ad5837
Daniel Rice <dlrice@ebi.ac.uk>
2024-01-29 09:08:55 +0000

    Add renderWithHistoryRouter helper.

---
+++ src/testHelpers/renderWithHistoryRouter.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '@testing-library/react';
+import { createBrowserHistory } from 'history';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+
+const renderWithHistoryRouter = (ui: ReactElement, route?: string) => {
+  const history = createBrowserHistory();
+  if (route) {
+    history.push(route);
+  }
+  console.log(history.location);
+  return {
+    ...render(<HistoryRouter history={history}>{ui}</HistoryRouter>),
+    history,
+  };
+};
+
+export default renderWithHistoryRouter;
