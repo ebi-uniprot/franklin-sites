7960c525f6d47f1994eaa4e738ce418684ceec50
Daniel Rice <dlrice@ebi.ac.uk>
2024-04-02 16:33:16 +0100

    Update svgo config to preserve viewBox.

--- svgo.config.js
+++ svgo.config.js
@@ -1,7 +1,15 @@
-module.export = {
+module.exports = {
+  multipass: true,
   plugins: [
     {
-      name: "removeViewBox"
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // viewBox is required to resize SVGs with CSS.
+          // @see https://github.com/svg/svgo/issues/1128
+          removeViewBox: false,
+        },
+      },
     },
-  ]
-}
+  ],
+};
