edbae939220004ae75562a8cf7d63ca387066f14
Daniel Rice <dlrice@ebi.ac.uk>
2024-04-05 12:18:19 +0100

    Fix BytesNumber story.

--- stories/BytesNumber.stories.tsx
+++ stories/BytesNumber.stories.tsx
@@ -1,13 +1,35 @@
-import { number } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { BytesNumber as BytesNumberComponent } from '../src/components';
 
-export default {
+const meta: Meta<typeof BytesNumberComponent> = {
+  component: BytesNumberComponent,
   title: 'Visualisation/Bytes number',
+  parameters: {
+    purposeFunction: {
+      purpose: '',
+      function: '',
+    },
+  },
+  argTypes: {
+    decimals: {
+      control: { type: 'number' },
+    },
+    children: {
+      control: { type: 'number' },
+      name: 'bytes',
+    },
+  },
+  args: {
+    children: 1024,
+  },
+  render: ({ decimals, children }) => (
+    <BytesNumberComponent decimals={decimals}>{children}</BytesNumberComponent>
+  ),
 };
 
-export const BytesNumber = () => (
-  <BytesNumberComponent decimals={number('decimals', 0)}>
-    {number('bytes', 1024)}
-  </BytesNumberComponent>
-);
+export default meta;
+
+type Story = StoryObj<typeof BytesNumberComponent>;
+
+export const Bubble: Story = {};
