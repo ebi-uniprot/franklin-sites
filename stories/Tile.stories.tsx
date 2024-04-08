53bbcd93814b335828df582472a902c4b8b22328
Daniel Rice <dlrice@ebi.ac.uk>
2024-04-08 10:40:52 +0100

    Fix Tile story.

--- stories/Tile.stories.tsx
+++ stories/Tile.stories.tsx
@@ -1,17 +1,24 @@
-import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { action } from '@storybook/addon-actions';
 
 import { loremIpsum } from 'lorem-ipsum';
 
-import { Tile } from '../src/components';
+import { Button, Tile as TileComponent } from '../src/components';
 
 import SVG from '../assets/uniprotkb_illustration.svg';
+
 import colors from '../src/styles/colours.json';
 
-export default {
+type StoryProps = React.ComponentProps<typeof TileComponent> & {
+  containerSize: string;
+  withBackgroundImage: boolean;
+  buttonInDescription: boolean;
+};
+
+const meta: Meta<StoryProps> = {
   title: 'Navigation/Tile',
-  decorators: [withKnobs()],
+  component: TileComponent,
   parameters: {
     purposeFunction: {
       purpose:
@@ -20,41 +27,63 @@ export default {
         'Provide a sneak peak and navigate to a searchable data section of the website.',
     },
   },
+  argTypes: {
+    headingLevel: {
+      control: 'select',
+      choices: ['h1', 'h2', 'h3', 'h4', 'h5'],
+    },
+    backgroundColor: {
+      control: 'select',
+      name: '--tile-background',
+      options: colors,
+    },
+  },
+  args: {
+    headingLevel: 'h3',
+    title: 'title',
+    subtitle: 'subtitle',
+    backgroundColor: colors.seaBlue,
+    containerSize: '40%',
+    buttonInDescription: false,
+  },
+  render: ({
+    title,
+    headingLevel,
+    containerSize,
+    withBackgroundImage,
+    subtitle,
+    gradient,
+    descriptionSlideUp,
+    buttonInDescription,
+    backgroundColor,
+  }) => (
+    <div style={{ width: containerSize }}>
+      <TileComponent
+        title={title}
+        headingLevel={headingLevel}
+        backgroundColor={backgroundColor}
+        backgroundImage={withBackgroundImage ? <SVG /> : null}
+        subtitle={subtitle}
+        gradient={gradient}
+        descriptionSlideUp={descriptionSlideUp}
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content
+        link={<a />}
+      >
+        {loremIpsum()}
+        {buttonInDescription && (
+          <div>
+            <Button onClick={action('description button clicked')}>
+              Some button
+            </Button>
+          </div>
+        )}
+      </TileComponent>
+    </div>
+  ),
 };
 
-export const BasicTile = () => (
-  <div style={{ width: text('Container size', '40%', 'ContainerProps') }}>
-    <Tile
-      title={text('title', 'Title', 'Props')}
-      headingLevel={select(
-        'headingLevel',
-        ['h1', 'h2', 'h3', 'h4', 'h5'],
-        'h3',
-        'Props'
-      )}
-      backgroundColor={select(
-        '--tile-background',
-        colors,
-        colors.seaBlue,
-        'Custom Properties'
-      )}
-      backgroundImage={boolean('with background?', false) ? <SVG /> : null}
-      subtitle={text('subtitle', 'subtitle', 'Props')}
-      gradient={boolean('gradient', false, 'Props')}
-      descriptionSlideUp={boolean('Slide up description', false, 'Props')}
-      // eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content
-      link={<a />}
-    >
-      {loremIpsum()}
-      {boolean('button in description', false) ? (
-        <button
-          type="button"
-          onClick={action('description button clicked')}
-          style={{ color: 'currentcolor', background: 'black' }}
-        >
-          Some button
-        </button>
-      ) : null}
-    </Tile>
-  </div>
-);
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const BasicTile: Story = {};
