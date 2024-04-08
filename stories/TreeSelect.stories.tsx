b5b30c24c284ca807fb71fb07b54b11ee67a5f58
Daniel Rice <dlrice@ebi.ac.uk>
2024-04-08 10:47:15 +0100

    Fix TreeSelect story.

--- stories/TreeSelect.stories.tsx
+++ stories/TreeSelect.stories.tsx
@@ -1,44 +1,56 @@
+import { Meta, StoryObj } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, select, text } from '@storybook/addon-knobs';
 
 import { TreeSelect as TS } from '../src/components';
 
 import { treeData } from '../src/mock-data/tree-data';
 
-export default {
+const meta: Meta<typeof TS> = {
   title: 'Forms/Tree Select',
-  decorators: [withKnobs()],
+  component: TS,
   parameters: {
     purposeFunction: {
       purpose: 'Allow selection of item(s) from nested data set',
       function: 'Navigate through a tree to make a selection',
     },
   },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'tertiary'],
+    },
+  },
+  args: {
+    label: 'Select',
+    variant: 'primary',
+  },
 };
 
-const useLabel = () => text('label', 'Select', 'Props');
+export default meta;
 
-const variants = ['primary', 'secondary', 'tertiary'] as const;
+type Story = StoryObj<typeof TS>;
 
-const useVariant = () => select('variant', variants, 'primary', 'Props');
+export const TreeSelect: Story = {
+  render: ({ label, variant }) => (
+    <TS
+      label={label}
+      data={treeData}
+      onSelect={action('onSelect')}
+      variant={variant}
+    />
+  ),
+};
 
-export const TreeSelect = () => (
-  <TS
-    label={useLabel()}
-    data={treeData}
-    onSelect={action('onSelect')}
-    variant={useVariant()}
-  />
-);
-
-export const TreeSelectWithAutocomplete = () => (
-  <TS
-    label={useLabel()}
-    data={treeData}
-    onSelect={action('onSelect')}
-    autocomplete
-    autocompletePlaceholder="Search for item"
-    autocompleteFilter
-    variant={useVariant()}
-  />
-);
+export const TreeSelectWithAutocomplete: Story = {
+  render: ({ label, variant }) => (
+    <TS
+      label={label}
+      data={treeData}
+      onSelect={action('onSelect')}
+      autocomplete
+      autocompletePlaceholder="Search for item"
+      autocompleteFilter
+      variant={variant}
+    />
+  ),
+};
