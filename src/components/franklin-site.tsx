d7b880ebfb499e6c95b01fb9399ee88c945e1b91
Aurélien Luciani <aurel-l@users.noreply.github.com>
2024-09-23 15:01:20 +0100

    CSS file optimisation for smaller builds

--- src/components/franklin-site.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-
-import '../styles/index.scss';
-
-const FranklinSite: FC<PropsWithChildren> = ({ children }) => <>{children}</>;
-
-export default FranklinSite;
