e306e9d441bb8b263059f91a37d07e5d34d00de5
Daniel Rice <dlrice@ebi.ac.uk>
2024-10-14 11:13:28 +0100

    Add minimum/maximum number of sequences to sequence submission tests.

--- src/components/__tests__/sequence-submission.spec.tsx
+++ src/components/__tests__/sequence-submission.spec.tsx
@@ -4,6 +4,12 @@ import SequenceSubmission from '../sequence-submission';
 import { validResponse } from '../../sequence-utils/sequenceValidator';
 
 describe('SequenceSubmission', () => {
+  const multipleSequences = `> sequence_1
+ACTGUACTGUACTGU
+> sequence_2
+ACTGAUTTGUATTGUUUGU
+`;
+
   it('should render', () => {
     const { asFragment } = render(<SequenceSubmission />);
     expect(asFragment()).toMatchSnapshot();
@@ -46,5 +52,27 @@ describe('SequenceSubmission', () => {
         raw: value,
       },
     ]);
+  });
+
+  it('should render with too many sequences error', () => {
+    render(
+      <SequenceSubmission value={multipleSequences} maximumSequences={1} />
+    );
+    expect(
+      screen.getByText(
+        'Your input contains 2 sequences which exceeds the submission limit of 1.'
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('should render with too few sequences error', () => {
+    render(
+      <SequenceSubmission value={multipleSequences} minimumSequences={3} />
+    );
+    expect(
+      screen.getByText(
+        'Your input contains 2 sequences. At least 3 sequences are required.'
+      )
+    ).toBeInTheDocument();
   });
 });
