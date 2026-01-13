2f41142ec7182cc3704adc42315b47823d62334e
Daniel Rice <dlrice@ebi.ac.uk>
2026-01-13 14:00:52 +0000

    Re-enable onSelectionChange for DataList and DataTable.

--- stories/DataTable.stories.tsx
+++ stories/DataTable.stories.tsx
@@ -14,70 +14,115 @@ import {
 } from '../src/decorators/DataDecorator';
 import { type WrapperProps } from '../src/components/data-loader';
 
-type DataTableComponentAndWrapperProps = React.ComponentProps<
-  typeof DataTableComponent
-> &
-  WrapperProps<DataType>;
+type StoryProps = React.ComponentProps<typeof DataTableComponent> &
+  WrapperProps<DataType> & {
+    isSelectable: boolean;
+  };
 
-const meta: Meta<DataTableComponentAndWrapperProps> = {
+const meta: Meta<StoryProps> = {
   component: DataTableComponent,
   title: 'Data/Data Table',
   argTypes: {
-    clickToLoad: { control: 'boolean' },
+    onSelectionChange: {
+      table: {
+        disable: true,
+      },
+    },
+    isSelectable: {
+      name: 'Selectable (onSelectionChange)',
+      control: 'boolean',
+      description: 'Toggle to enable selection checkboxes',
+    },
+    clickToLoad: {
+      control: 'boolean',
+      description: 'Require a click to load data (when using loader)',
+    },
   },
   args: {
     onHeaderClick: fn(),
+    onSelectionChange: fn(),
+    isSelectable: true,
+    clickToLoad: false,
   },
 };
 
 export default meta;
 
-type Story = StoryObj<DataTableComponentAndWrapperProps>;
+type Story = StoryObj<StoryProps>;
 
 export const DataTable: Story = {
-  render: (storyProps) => (
-    <DataDecorator>
-      {(props: CommonProps) => (
-        <DataTableComponent {...props} {...storyProps} columns={columns} />
-      )}
-    </DataDecorator>
-  ),
+  argTypes: {
+    clickToLoad: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: (args) => {
+    const { isSelectable, onSelectionChange, clickToLoad, ...restArgs } = args;
+    const finalOnSelectionChange = isSelectable ? onSelectionChange : undefined;
+
+    return (
+      <DataDecorator>
+        {(props: CommonProps) => (
+          <DataTableComponent
+            {...props}
+            {...restArgs}
+            onSelectionChange={finalOnSelectionChange}
+            columns={columns}
+          />
+        )}
+      </DataDecorator>
+    );
+  },
 };
 
 export const DataTableWithLoader: Story = {
-  render: (storyProps) => (
-    <DataLoaderDecorator>
-      {(props: CommonProps & WrapperProps<DataType>) => (
-        <DataTableWithLoaderComponent
-          {...props}
-          {...storyProps}
-          columns={columns}
-        />
-      )}
-    </DataLoaderDecorator>
-  ),
+  render: (args) => {
+    const { isSelectable, onSelectionChange, ...restArgs } = args;
+    const finalOnSelectionChange = isSelectable ? onSelectionChange : undefined;
+
+    return (
+      <DataLoaderDecorator>
+        {(props: CommonProps & WrapperProps<DataType>) => (
+          <DataTableWithLoaderComponent
+            {...props}
+            {...restArgs}
+            onSelectionChange={finalOnSelectionChange}
+            columns={columns}
+          />
+        )}
+      </DataLoaderDecorator>
+    );
+  },
 };
 
 export const DataTableColumnLoading: Story = {
-  render: (storyProps) => (
-    <DataLoaderDecorator>
-      {(props: CommonProps & WrapperProps<DataType>) => (
-        <DataTableWithLoaderComponent
-          {...props}
-          {...storyProps}
-          loading
-          columns={[
-            ...columns,
-            {
-              label: 'Column 6',
-              name: 'content6',
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore we know it doesn't exist, it's on purpose
-              render: (row) => row.content6.complexValue,
-            },
-          ]}
-        />
-      )}
-    </DataLoaderDecorator>
-  ),
+  render: (args) => {
+    const { isSelectable, onSelectionChange, ...restArgs } = args;
+    const finalOnSelectionChange = isSelectable ? onSelectionChange : undefined;
+
+    return (
+      <DataLoaderDecorator>
+        {(props: CommonProps & WrapperProps<DataType>) => (
+          <DataTableWithLoaderComponent
+            {...props}
+            {...restArgs}
+            onSelectionChange={finalOnSelectionChange}
+            loading
+            columns={[
+              ...columns,
+              {
+                label: 'Column 6',
+                name: 'content6',
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore we know it doesn't exist, it's on purpose
+                render: (row) => row.content6.complexValue,
+              },
+            ]}
+          />
+        )}
+      </DataLoaderDecorator>
+    );
+  },
 };
