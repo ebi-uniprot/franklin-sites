897fe0d9838c9710551e8708382ad95c185b008b
UniProt CI <uniprotci@gmail.com>
2026-06-01 16:34:32 +0000

    Deploy gitlab.ebi.ac.uk/uniprot/uniprot-website/franklin-sites to github.com/ebi-uniprot/franklin-sites.git:gh-pages

---
+++ assets/ExternalLink.stories-I86Xbdbj.js
@@ -0,0 +1,18 @@
+import{o as e}from"./preload-helper-BKhVKv2_.js";import{ct as t,t as n}from"./components-BXRiKLRr.js";var r,i,a,o,s,c,l,u,d,f,p;e((()=>{n(),l={component:t,title:`Core/External link`},u={args:{url:`https://www.ebi.ac.uk/`,children:`external link`,noIcon:!1,tidyUrl:!1}},d={args:{...u.args,children:void 0}},f={args:{...u.args,url:null}},u.parameters={...u.parameters,docs:{...(r=u.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    url: 'https://www.ebi.ac.uk/',
+    children: 'external link',
+    noIcon: false,
+    tidyUrl: false
+  }
+}`,...(i=u.parameters)==null||(i=i.docs)==null?void 0:i.source}}},d.parameters={...d.parameters,docs:{...(a=d.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    ...ExternalLink.args,
+    children: undefined
+  }
+}`,...(o=d.parameters)==null||(o=o.docs)==null?void 0:o.source}}},f.parameters={...f.parameters,docs:{...(s=f.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...ExternalLink.args,
+    url: null
+  }
+}`,...(c=f.parameters)==null||(c=c.docs)==null?void 0:c.source}}},p=[`ExternalLink`,`ExternalLinkWithoutPassingText`,`ExternalLinkWithNullUrl`]}))();export{u as ExternalLink,f as ExternalLinkWithNullUrl,d as ExternalLinkWithoutPassingText,p as __namedExportsOrder,l as default};
---
+++ assets/react-DowHYz2M.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-CM0ao9Nf.js";t();export{e as MDXProvider};
