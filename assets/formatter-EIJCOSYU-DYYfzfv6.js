2336031d3dc9ac1001de101a5006f637c895f72c
UniProt CI <uniprotci@gmail.com>
2026-06-19 13:58:31 +0000

    Deploy gitlab.ebi.ac.uk/uniprot/uniprot-website/franklin-sites to github.com/ebi-uniprot/franklin-sites.git:gh-pages

--- assets/formatter-EIJCOSYU-DYYfzfv6.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./preload-helper-BKhVKv2_.js";import{F as t,P as n}from"./iframe-BfRUjcDD.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-Dr0iD2RY.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
